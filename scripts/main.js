0578dae0b3bab892151dfeb6592f58d00b4b28a8
Adri <a.fernandezrodriguez2003@gmail.com>
2023-12-04 10:16:48 +0100

    Continuamos con el proyecto

--- scripts/main.js
+++ scripts/main.js
@@ -9,7 +9,7 @@ export function mostrarResultado(mensaje){
 }
 
 const vehiculo1 = new Vehiculo('BMW', 'CLK', 'rojo', 1992, '2400')
-mostrarResultado('Vehiculo 1:')
+mostrarResultado('Vehiculo 1:<hr/>')
 vehiculo1.mostrarDatos()
 vehiculo1.arrancar()
 vehiculo1.acelerar(140)
@@ -28,12 +28,14 @@ localStorage.setItem("vehiculo", JSON.stringify(vehiculoObject))
 console.log(JSON.parse(localStorage.getItem("vehiculo")))
 
 
+
 const deportivo1 = new AutomovilDeportivo('Ferrari', 'Spider', 'amarillo', 2020, '4000', '500')
-mostrarResultado('<br/><br/>Deportivo 1:')
+mostrarResultado('<br/><br/>Deportivo 1:<hr/>')
 deportivo1.mostrarDatos()
 deportivo1.arrancar()
 deportivo1.acelerar(140)
 deportivo1.frenar()
+
 
 const deportivoObject = {
     marca: 'Ferrari',
@@ -43,6 +45,7 @@ const deportivoObject = {
     cilindrada: '4000',
     potencia: '500'
 }
+
 
 for(let key in deportivoObject){
     if(deportivoObject.hasOwnProperty){
